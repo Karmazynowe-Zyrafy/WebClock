84ee71625b5ac3d95b88b0ff51d04dd6b3eecbb2
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-03 14:05:41 +0200

    Add controller Clock

---
+++ WebClock/Controllers/ClockController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebClock.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClockController: ControllerBase
+    {
+        
+        [HttpGet]
+        public DateTime Get()
+        {
+            return DateTime.Now;
+        }
+        
+        
+    }
+}
