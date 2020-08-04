9eea20ad0e0406eec6205f717fe986abcf82c3df
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-04 15:14:08 +0200

    Test ReturnCorrectTime now works properly

--- WebColock.Tests/ClockControllerTests.cs
+++ WebColock.Tests/ClockControllerTests.cs
@@ -15,9 +15,7 @@ namespace WebColock.Tests
         {
             var clockController = new ClockController();
             var result = clockController.GetCurrentDate();
-
             Assert.IsType<string>(result);
-            
         }
         [Fact]
         public void GetCurrentDate_WhenCalled_ReturnCorrectTime()
@@ -26,8 +24,10 @@ namespace WebColock.Tests
             var result = clockController.GetCurrentDate();
 
             var currentDate= JsonConvert.DeserializeObject<Clock>(result);
-          // c.Hour=
-            //Assert.InRange(DateTime.Parse(json),DateTime.Now-TimeSpan.FromSeconds(5),DateTime.Now-TimeSpan.FromSeconds(-5) );
+            if (currentDate.Hour==DateTime.Now.Hour&&currentDate.Hour==DateTime.Now.Hour)
+            {
+                Assert.InRange(DateTime.Now.Second,currentDate.Second-15,currentDate.Second+15);
+            }
         }
     }
 }
