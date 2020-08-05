a718914467a1ee3ac75bfadb0f090440cb0e573f
Józef Salik <jozek243@gmail.com>
2020-08-05 08:31:50 +0200

    Add test GetCurrentTime_WhenCalled_ReturnCorrectTimeInterval() and refactoring.

--- WebColock.Tests/ClockControllerTests.cs
+++ WebColock.Tests/ClockControllerTests.cs
@@ -8,24 +8,42 @@ namespace WebColock.Tests
 {
     public class ClockControllerTests
     {
+
+        private readonly ClockController _controller;
+
+        public ClockControllerTests()
+        {
+            _controller = new ClockController();
+        }
+
         [Fact]
         public void GetCurrentTime_WhenCalled_ReturnString()
         {
-            var clockController = new ClockController();
-            var result = clockController.GetCurrentTime();
+            var result = _controller.GetCurrentTime();
             Assert.IsType<string>(result);
         }
 
         [Fact]
         public void GetCurrentTime_WhenCalled_ReturnCorrectSecond()
         {
-            var clockController = new ClockController();
-            var result = clockController.GetCurrentTime();
+            var result = _controller.GetCurrentTime();
 
             var currentDate= JsonConvert.DeserializeObject<Clock>(result);
           
                 Assert.InRange(DateTime.Now.Second,currentDate.Second-15,currentDate.Second+15);
             
+        }
+
+        [Fact]
+        public void GetCurrentTime_WhenCalled_ReturnCorrectTimeInterval()
+        {
+            var result = _controller.GetCurrentTime();
+
+            var currentDate = JsonConvert.DeserializeObject<Clock>(result);
+
+            Assert.InRange(DateTime.Now.Hour, 0,23);
+            Assert.InRange(DateTime.Now.Minute, 0,59);
+            Assert.InRange(DateTime.Now.Second, 0,59);
         }
     }
 }
