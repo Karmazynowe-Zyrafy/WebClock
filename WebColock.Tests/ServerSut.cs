8c6f85f7060bca56c1eb63d9b5477c9c914b1682
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-06 11:04:17 +0200

    Initialization Integration Test

---
+++ WebColock.Tests/ServerSut.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using WebClock;
+
+namespace WebColock.Tests
+{
+    public class ServerSut
+    {
+        public HttpClient Client { get; }
+
+        public ServerSut()
+        {
+            var webApplication = new WebApplicationFactory<Startup>();
+            Client = webApplication.WithWebHostBuilder(builder =>
+            {
+                builder.UseEnvironment("Debug");
+                builder.UseStartup<Startup>();
+            }).CreateClient();
+        }
+
+        public async Task<T> DoGet<T>(string path)
+        {
+            var result = await Client.GetAsync(path);
+            var content = await result.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(content);
+        }
+    }
+}
