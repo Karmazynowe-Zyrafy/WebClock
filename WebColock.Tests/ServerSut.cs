c6c00e871f38ebe2b75c0b5950cfe77b394c4239
Józef Salik <jozek243@gmail.com>
2020-08-06 14:05:12 +0200

    Change in .gitignore.

--- WebColock.Tests/ServerSut.cs
+++ WebColock.Tests/ServerSut.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
