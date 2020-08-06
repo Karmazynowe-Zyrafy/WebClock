e9c3e534aafad7af1637687c8f88eed65259c337
Józef Salik <jozek243@gmail.com>
2020-08-06 11:52:43 +0200

    Add FluentAssertionss NuGet.

--- WebColock.Tests/IntegrationTest/IntegrationClockControllerTests.cs
+++ WebColock.Tests/IntegrationTest/IntegrationClockControllerTests.cs
@@ -1,7 +1,18 @@
-﻿namespace WebColock.Tests.IntegrationTest
+﻿using System;
+using Newtonsoft.Json;
+using WebClock;
+using Xunit;
+
+namespace WebColock.Tests.IntegrationTest
 {
     public class IntegrationClockTest
     {
-        
+        [Fact]
+        public async void GetCurrentTime_WhenCalled_ReturnCorrectSecond()
+        {
+            var server = new ServerSut();
+            var result = await server.DoGet<Clock>("/clock");
+
+        }
     }
 }
--- WebColock.Tests/ServerSut.cs
+++ WebColock.Tests/ServerSut.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
