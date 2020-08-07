89b0db5f1596b5bd32d51436f488ca16673abb0b
BuchyPL <62808706+BuchyPL@users.noreply.github.com>
2020-08-07 10:43:23 +0200

    added Clockout data model

---
+++ WebClock/Models/Clockout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace WebClock.Models
+{
+    public class Clockout
+    {
+        public int UserId { get; set; }
+        public DateTime ClockoutTime { get; set; }
+        public string ClockoutStatus { get; set; }
+    }
+}
