5f1d1e15b665e0482814628b3faa56c832795817
Józef Salik <jozek243@gmail.com>
2020-08-10 10:14:16 +0200

    Changes in class ClockInOutSingleton.

---
+++ WebClock/Models/ClockInOutSingleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebClock.Models
+{
+    public class ClockInOutSingleton
+    {
+        private static ClockInOutSingleton _instance = new ClockInOutSingleton();
+
+        private ClockInOutSingleton()
+        {
+            ClockInOutList = new List<ClockInOut> {
+                new ClockInOut { UserId = 1, ClockoutTime = DateTime.UtcNow, ClockoutStatus = "in" },
+                new ClockInOut { UserId = 2, ClockoutTime = DateTime.UtcNow.AddHours(2), ClockoutStatus = "out" },
+                new ClockInOut { UserId = 3, ClockoutTime = DateTime.UtcNow.AddHours(4), ClockoutStatus = "in" }
+            };
+        }
+
+        public static List<ClockInOut> ClockInOutList;
+
+        public static ClockInOutSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ClockInOutSingleton();
+                }
+                return _instance;
+            }
+        }
+
+        public List<ClockInOut> GetClockInOutSingleton()
+        {
+            return ClockInOutList;
+        }
+        
+        public List<ClockInOut> ChangeClockStatus(int id)
+        {
+            //todo
+            return ClockInOutList;
+        }
+    }
+}
--- WebClock/Models/Clockinout.cs
+++ WebClock/Models/Clockinout.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace WebClock.Models
 {
@@ -8,31 +7,5 @@ namespace WebClock.Models
         public int UserId { get; set; }
         public DateTime ClockoutTime { get; set; }
         public string ClockoutStatus { get; set; }
-    }
-
-    public class ClockInOutSingleton
-    {
-        private static ClockInOutSingleton instance = new ClockInOutSingleton();
-        private ClockInOutSingleton() { }
-        public static ClockInOutSingleton Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new ClockInOutSingleton();
-                }
-                return instance;
-            }
-        }
-        public List<ClockInOut> GetClockInOutSingleton()
-        {
-            var clockInOutList = new List<ClockInOut> {
-            new ClockInOut { UserId = 1, ClockoutTime = DateTime.UtcNow, ClockoutStatus = "in" },
-            new ClockInOut { UserId = 1, ClockoutTime = DateTime.UtcNow.AddHours(2), ClockoutStatus = "out" },
-            new ClockInOut { UserId = 1, ClockoutTime = DateTime.UtcNow.AddHours(4), ClockoutStatus = "in" }
-            };
-            return clockInOutList;
-        }
     }
 }
