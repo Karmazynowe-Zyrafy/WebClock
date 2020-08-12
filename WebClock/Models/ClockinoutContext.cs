4e47479d1d9f0c964ab94398cedd5242c2ffe630
BuchyPL <62808706+BuchyPL@users.noreply.github.com>
2020-08-12 09:03:37 +0200

    Added ClockinoutContext class and installed nuget Microsoft.EntityFrameworkCore

---
+++ WebClock/Models/ClockinoutContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClock.Models
+{
+    public class ClockinoutContext : DbContext
+    {
+        public ClockinoutContext(DbContextOptions<ClockinoutContext>options):base(options)
+        {
+
+        }
+        public DbSet<ClockInOut> clockInOuts { get; set; }
+    }
+}
