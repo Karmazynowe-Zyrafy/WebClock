454b21750bd21426a8af66c11c5650d66cfcb754
Józef Salik <jozek243@gmail.com>
2020-08-17 12:42:03 +0200

    Delete files.

---
+++ WebColock.Tests/IntegrationTest/IntegrationClockInOutControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace WebColock.Tests.IntegrationTest
+{
+    public class IntegrationClockInOutControllerTests
+    {
+        [Fact]
+        public async void ClockIn_Test()
+        {
+            var server = new ServerSut();
+            var userId = 1;
+
+            await server
+                .DoPost<object>($"api/ClockInOut/ClockIn/{userId}", new object());
+            //todo usunąć <object>
+        }
+    }
+}
