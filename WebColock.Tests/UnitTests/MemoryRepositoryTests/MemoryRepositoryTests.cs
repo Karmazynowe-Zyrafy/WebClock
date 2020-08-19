fc0b9dc7b1fd47cccd6cc41e7c06ca124f826b53
Karol Węglarz <49003111+Czadzik@users.noreply.github.com>
2020-08-19 15:57:44 +0200

    Add MemoryRepository read test

--- WebColock.Tests/UnitTests/MemoryRepositoryTests/MemoryRepositoryTests.cs
+++ WebColock.Tests/UnitTests/MemoryRepositoryTests/MemoryRepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using FluentAssertions;
 using WebClock.Models;
 using WebClock.Models.MemoryRepository;
@@ -20,8 +21,27 @@ namespace WebColock.Tests.UnitTests.MemoryRepositoryTests
             };
 
             memoryRepository.Write(clockInOut);
-            
+
             memoryRepository.ClocksInOut.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void Read_WhenCalled_ReturnClockInOutObject()
+        {
+            var memoryRepository = new MemoryRepository();
+            var date = DateTime.Now;
+            var clockInOut = new ClockInOut
+            {
+                UserId = 1,
+                Date = date,
+                Type = ClockType.In
+            };
+            memoryRepository.Write(clockInOut);
+
+            var result = memoryRepository.Read(1);
+            result.Select(x => x.UserId).FirstOrDefault().Should().Be(1);
+            result.Select(x => x.Type).FirstOrDefault().Should().Be(ClockType.In);
+            result.Select(x => x.Date).FirstOrDefault().Should().Be(date);
         }
     }
 }
