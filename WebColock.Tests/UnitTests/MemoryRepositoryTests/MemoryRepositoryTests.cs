e9d2f27cfba0b6e1ed1a56ca41ed1e6691d15836
Józef Salik <jozek243@gmail.com>
2020-08-19 15:45:33 +0200

    Add unit test for Write() in MemoryRepository class.

---
+++ WebColock.Tests/UnitTests/MemoryRepositoryTests/MemoryRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using WebClock.Models;
+using WebClock.Models.MemoryRepository;
+using Xunit;
+
+namespace WebColock.Tests.UnitTests.MemoryRepositoryTests
+{
+    public class MemoryRepositoryTests
+    {
+        [Fact]
+        public void Write_WhenCalled_AddClockInOutObject()
+        {
+            var memoryRepository = new MemoryRepository();
+            var clockInOut = new ClockInOut
+            {
+                UserId = 1,
+                Date = DateTime.Now,
+                Type = ClockType.In
+            };
+
+            memoryRepository.Write(clockInOut);
+            
+            memoryRepository.ClocksInOut.Should().HaveCount(1);
+        }
+    }
+}
